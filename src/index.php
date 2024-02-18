0d17f27066c650ba73baab36735ac730bb3f3c76
RNY <rennya090611@gmail.com>
2024-02-19 00:48:51 +0900

    css調整

--- src/index.php
+++ src/index.php
@@ -3,12 +3,14 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="scss/dist/header.css">
+    <link rel="stylesheet" href="scss/dist/footer.css">
     <link rel="stylesheet" href="scss/dist/top.css">
     <title>PHP マニュアル</title>
 </head>
 <body>
-    <header class="header">
-        <h1 header__title>PHPマニュアル勉強用</h1>
+    <header class="l-header">
+        <h1 class="l-header__title">PHPマニュアル勉強用</h1>
     </header>
     <main>
     </main>
